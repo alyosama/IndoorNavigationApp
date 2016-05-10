32c98a063d9def3a463b527362ca86afd56db105
FatmaGamal <hermonie.23@gmail.com>
2016-05-11 01:12:47 +0200

    Main Activity UI

--- app/src/main/java/com/example/aly/indoornavigationapp/Configure.java
+++ app/src/main/java/com/example/aly/indoornavigationapp/Configure.java
@@ -9,27 +9,20 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
-import java.util.logging.Handler;
 
-import android.os.HandlerThread;
-
-public class Settings extends AppCompatActivity {
+public class Configure extends AppCompatActivity {
     DatabaseHelper db;
     WifiManager mainWifi;
     WifiReceiver receiverWifi;
@@ -96,7 +89,7 @@ public class Settings extends AppCompatActivity {
         configureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Settings.this, WAP.class);
+                Intent i = new Intent(Configure.this, WAP.class);
                 startActivity(i);
 
             }
@@ -121,7 +114,7 @@ public class Settings extends AppCompatActivity {
         placeConfButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Settings.this, Places.class);
+                Intent i = new Intent(Configure.this, Places.class);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/aly/indoornavigationapp/MainActivity.java
+++ app/src/main/java/com/example/aly/indoornavigationapp/MainActivity.java
@@ -4,95 +4,104 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.Cursor;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.os.Handler;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    TextView mainText;
-    WifiManager mainWifi;
-    WifiReceiver receiverWifi;
-    List<ScanResult> wifiList;
-    StringBuilder sb = new StringBuilder();
+    Button findPathBtn;
+    Spinner places;
+    DatabaseHelper helper;
+    List<String> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button postBtn=(Button)findViewById(R.id.postBtn);
 
-        mainText=(TextView)findViewById(R.id.textView);
+        helper = new DatabaseHelper(this);
 
-        // Initiate wifi service manager
-        mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        postBtn.setOnClickListener(new View.OnClickListener() {
+
+        findPathBtn = (Button)findViewById(R.id.pathBtn);
+        places = (Spinner) findViewById(R.id.spinner);
+        list = new ArrayList<String>();
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_spinner_item,list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        places.setAdapter(dataAdapter);
+
+        findPathBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                // Check for wifi is disabled
-                if (mainWifi.isWifiEnabled() == false)
-                {
-                    // If wifi disabled then enable it
-                    Toast.makeText(getApplicationContext(), "wifi is disabled..making it enabled",
-                            Toast.LENGTH_LONG).show();
-
-                    mainWifi.setWifiEnabled(true);
+            public void onClick(View view) {
+                findPath();
+            }
+        });
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                handler.postDelayed(this,2000);
+                getLocation();
+                Cursor cursor = helper.fetchAllPlaces();
+                if(cursor != null) {
+                    while (!cursor.isAfterLast()) {
+                        list.add(cursor.getString(0));
+                        cursor.moveToNext();
+                    }
                 }
-
-                // wifi scaned value broadcast receiver
-                receiverWifi = new WifiReceiver();
-
-                // Register broadcast receiver
-                // Broacast receiver will automatically call when number of wifi connections changed
-                registerReceiver(receiverWifi, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
-                mainWifi.startScan();
-                mainText.setText("Starting Scan...");
-
             }
-        });
-
-        Button settingsBtn = (Button) findViewById(R.id.settingsBtn);
-        settingsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Settings.class);
-                startActivity(intent);
-            }
-        });
+        };
+        runnable.run();
     }
-    class WifiReceiver extends BroadcastReceiver {
 
-        // This method call when number of wifi connections changed
-        public void onReceive(Context c, Intent intent) {
+    public int getLocation() {
+        Random n = new Random();
+        return n.nextInt(15)+1;
+    }
 
-            sb = new StringBuilder();
-            wifiList = mainWifi.getScanResults();
-            sb.append("\n        Number Of Wifi connections :"+wifiList.size()+"\n\n");
+    public void findPath(){
+        String destination = String.valueOf(places.getSelectedItem());
+    }
 
-            for(int i = 0; i < wifiList.size(); i++){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.setting_menu, menu);
+        return true;
+    }
 
-                sb.append(new Integer(i+1).toString() + ". "+wifiList.get(i).SSID+" : ");
-                double distance=calculateDistance(wifiList.get(i).level,wifiList.get(i).frequency);
-                sb.append(String.format("%.4f", distance));
-                sb.append(" meters\n\n");
-            }
-
-
-            mainText.setText(sb);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent i = null;
+        switch (item.getItemId()) {
+            case R.id.places:
+                i = new Intent(this, Places.class);
+                startActivity(i);
+                return true;
+            case R.id.wap:
+                i = new Intent(this, WAP.class);
+                startActivity(i);
+                return true;
+            case R.id.configure:
+                i = new Intent(this, Configure.class);
+                startActivity(i);
+                return true;
         }
-
-        // Calcuate Distance From WIFI Access Point
-        public double calculateDistance(double signalLevelInDb,double freqInMHz){
-            double exp=(27.55-(20*Math.log10(freqInMHz))+Math.abs(signalLevelInDb))/20.0;
-            return Math.pow(10.0,exp);
-        }
-
+        return false;
     }
 
 }
